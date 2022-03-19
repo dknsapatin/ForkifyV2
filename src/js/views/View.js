65c46403fd1f24d2d54ee77aa74bb493a4157fa6
dknsapatin <dknsapatin@gmail.com>
2022-03-19 00:03:52 -0400

    Finalized forkify for deployment

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Jonas Schmedtmann
+   * @todo Finish implementation
+   */
+
   // ///////////////////////////////////////////////////////////////////////////////
   render(data, render = true) {
     // if there is no data || or if there is data but the length is 0
@@ -70,14 +80,16 @@ export default class View {
   // ///////////////////////////////////////////////////////////////////////////////
   // Displaying Error message if model.js cannot find recipe
   renderError(message = this._errorMessage) {
-    const markup = ` <div class="error">
-    <div>
-      <svg>
-        <use href="${icons}#icon-alert-triangle"></use>
-      </svg>
-    </div>
-    <p>${message}</p>
-  </div> `;
+    const markup = `
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
@@ -85,14 +97,16 @@ export default class View {
   // ///////////////////////////////////////////////////////////////////////////////
   // Displaying Success message if model.js finds recipe
   renderMessage(message = this._message) {
-    const markup = ` <div class="message">
-    <div>
-      <svg>
-        <use href="${icons}#icon-smile"></use>
-      </svg>
-    </div>
-    <p>${message}</p>
-  </div> `;
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
